afcd210d7c807d026cc15f1ba6d7eaabf7f1c62d
Maxim Kostin <maxim.kostin.1999@gmail.com>
2025-11-05 15:30:48 +0500

    Добавьте файлы проекта.

---
+++ DictionatiesForEverything/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DictionatiesForEverything
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
